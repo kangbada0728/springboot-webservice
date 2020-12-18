4f4be75f5076a8dfc9601e8c69e5623ef2e2a949
Bada Kang <kangbada0728@gmail.com>
2020-12-18 23:28:40 +0900

    JPA 적용 - 1

---
+++ src/main/java/com/kangbada/practice/springboot/domain/posts/Posts.java
@@ -0,0 +1,23 @@
+package com.kangbada.practice.springboot.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+
+
+
+}
