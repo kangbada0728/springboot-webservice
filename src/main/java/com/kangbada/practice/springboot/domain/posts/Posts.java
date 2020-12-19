1f1f5fc5bc9c1060742f43486f7337ac644ff2c5
Bada Kang <kangbada0728@gmail.com>
2020-12-19 18:16:08 +0900

    Ch3. 스프링 부트에서 JPA로 데이터베이스 다뤄보자. 완료

--- src/main/java/com/kangbada/practice/springboot/domain/posts/Posts.java
+++ src/main/java/com/kangbada/practice/springboot/domain/posts/Posts.java
@@ -1,5 +1,7 @@
 package com.kangbada.practice.springboot.domain.posts;
 
+import com.kangbada.practice.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,7 +19,21 @@ public class Posts {
     @Column(length = 500, nullable = false)
     private String title;
 
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 
+    private String author;
 
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content= content;
+    }
 
 }
