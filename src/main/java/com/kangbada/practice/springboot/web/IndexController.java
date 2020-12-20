d9640527fcfb33389a604c481e16b013029e69df
Bada Kang <kangbada0728@gmail.com>
2020-12-20 15:53:11 +0900

    Ch 5.1~5.5 세션 저장소로 데이터베이스 사용하기까지 완료

--- src/main/java/com/kangbada/practice/springboot/web/IndexController.java
+++ src/main/java/com/kangbada/practice/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kangbada.practice.springboot.web;
 
+import com.kangbada.practice.springboot.config.auth.LoginUser;
+import com.kangbada.practice.springboot.config.auth.dto.SessionUser;
 import com.kangbada.practice.springboot.service.posts.PostsService;
 import com.kangbada.practice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,4 +39,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
