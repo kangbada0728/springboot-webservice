a2a8966baba697bee2c9ecfd973e9e73028a7997
Bada Kang <kangbada0728@gmail.com>
2020-12-24 16:09:09 +0900

    [완료] Ch06 AWS 서버 환경을 만들어보자 - AWS EC2

---
+++ src/main/java/com/kangbada/practice/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kangbada.practice.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
